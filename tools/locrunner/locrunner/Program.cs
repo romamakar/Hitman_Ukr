7586554461133c154349a73e19f17c192492670d
Roman <Roman@SURFACEPC>
2025-02-09 23:39:48 +0200

    Add project files.

---
+++ tools/locrunner/locrunner/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+string currentDirectory = Directory.GetCurrentDirectory();
+string[] locFiles = Directory.GetFiles(currentDirectory, "*.loc");
+
+foreach (string locFile in locFiles)
+{
+    string fileName = Path.GetFileName(locFile);
+    string outputJsonFile = Path.ChangeExtension(fileName, ".json");
+    Process process = new Process()
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = "locc",
+            Arguments = $"--from=\"{locFile}\" --to=\"{outputJsonFile}\" --mode=decompile",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        }
+    };
+
+    Console.WriteLine($"Processing: {locFile} -> {outputJsonFile}");
+    process.Start();
+    process.WaitForExit();
+
+    string output = process.StandardOutput.ReadToEnd();
+    string error = process.StandardError.ReadToEnd();
+
+    if (!string.IsNullOrEmpty(output))
+        Console.WriteLine(output);
+    if (!string.IsNullOrEmpty(error))
+        Console.WriteLine($"Error: {error}");
+}
