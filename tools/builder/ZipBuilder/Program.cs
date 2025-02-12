b9c96418089c66ab2dced047c7cb2bf09346c01a
romamakar <roman.makarevych@gmail.com>
2025-02-12 20:55:39 +0200

    added removing loc folder

--- tools/builder/ZipBuilder/Program.cs
+++ tools/builder/ZipBuilder/Program.cs
@@ -19,6 +19,8 @@ namespace ZipBuilder
             CreateMainLocks();
             CreatePostMissionLocs();
             UpdateZip();
+
+            Directory.Delete(localPath + "\\loc", true);
         }
 
         static void UpdateZip()
