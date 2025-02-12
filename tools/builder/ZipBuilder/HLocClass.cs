47b62e92b6b2c27cd6ab4e41fe08f4413f2745d2
romamakar <roman.makarevych@gmail.com>
2025-02-12 20:51:05 +0200

    restructured all files

---
+++ tools/builder/ZipBuilder/HLocClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipBuilder
+{
+    public class HLocClass
+    {
+        public string name { get; set; }
+        public byte? org_tbyte { get; set; }
+        public string value { get; set; }
+        public List<HLocClass> children { get; set; }
+        public int numChildren { get; set; }
+        public string type { get; set; }
+
+    }
+}
--- tools/builder/ZipBuilder/Program.cs
+++ tools/builder/ZipBuilder/Program.cs
@@ -1,13 +1,30 @@
-﻿using System.IO.Compression;
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Text;
 
 namespace ZipBuilder
 {
     internal class Program
     {
+        static string localPath = "C:\\Users\\roman\\OneDrive\\Desktop\\Hitman_Ukr";
         static void Main()
         {
-            string sourceFolder = @"C:\Users\roman\OneDrive\Desktop\Hitman_Ukr\loc"; // Папка з .loc файлами
-            string archiveFolder = @"C:\Users\roman\OneDrive\Desktop\Hitman_Ukr\hitman";  // Папка з архівами
+            if (!Directory.Exists(localPath + "\\loc"))
+            {
+                Directory.CreateDirectory(localPath + "\\loc");
+            }
+
+            ReplaceMainNodeFromBloodMoneyFileToAllFiles();
+            CreateMainLocks();
+            CreatePostMissionLocs();
+            UpdateZip();
+        }
+
+        static void UpdateZip()
+        {
+            string sourceFolder = localPath + @"\loc"; // Папка з .loc файлами
+            string archiveFolder = localPath + @"\release-sources";  // Папка з архівами
 
             if (!Directory.Exists(sourceFolder) || !Directory.Exists(archiveFolder))
             {
@@ -35,7 +52,7 @@ namespace ZipBuilder
                     entryPath = Path.Combine("Scenes", Path.GetFileName(filePath));
                 }
 
-                entryPath = entryPath.Replace("\\", "/");              
+                entryPath = entryPath.Replace("\\", "/");
 
                 if (!File.Exists(archivePath))
                 {
@@ -52,5 +69,186 @@ namespace ZipBuilder
                 Console.WriteLine($"Файл {filePath} додано до архіву {archivePath}");
             }
         }
+        static void CreateMainLocks()
+        {
+            string jsonDirectory = localPath + @"\json";
+            string locDirectory = localPath + @"\loc";
+            string[] jsonFiles = Directory.GetFiles(jsonDirectory, "*.json");
+
+            foreach (string jsonFile in jsonFiles)
+            {
+
+                string fileName = Path.GetFileName(jsonFile);
+                if (fileName.ToUpperInvariant() == "M00_MAIN.JSON")
+                {
+                    continue;
+                }
+                string outputLocFile = Path.ChangeExtension(fileName, ".loc");
+                Process process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "locc",
+                        Arguments = $"--from=\"{jsonDirectory}\\{fileName}\" --to=\"{locDirectory}\\{outputLocFile}\" --mode=compile", //--mode=decompile --pretty-json=true
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                Console.WriteLine($"Processing: {fileName} -> {outputLocFile}");
+                process.Start();
+                process.WaitForExit();
+
+                string output1 = process.StandardOutput.ReadToEnd();
+                string error1 = process.StandardError.ReadToEnd();
+
+                if (!string.IsNullOrEmpty(output1))
+                    Console.WriteLine(output1);
+                if (!string.IsNullOrEmpty(error1))
+                    Console.WriteLine($"Error: {error1}");
+            }
+
+            //M00_MAIN.JSON
+            string locFileM00 = locDirectory + "\\" + $"M00_main.loc";
+            string jsonFileM00 = jsonDirectory + "\\" + $"M00_main.json";
+            Process process2 = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "hitmanbe",
+                    Arguments = $"\"{jsonFileM00}\" \"{locFileM00}\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            Console.WriteLine($"Processing: {jsonFileM00} -> {locFileM00}");
+            process2.Start();
+            process2.WaitForExit();
+
+            string output = process2.StandardOutput.ReadToEnd();
+            string error = process2.StandardError.ReadToEnd();
+
+            if (!string.IsNullOrEmpty(output))
+                Console.WriteLine(output);
+            if (!string.IsNullOrEmpty(error))
+                Console.WriteLine($"Error: {error}");
+
+
         }
+
+        static void CreatePostMissionLocs()
+        {
+            string postmission = localPath + @"\json\postmission\postmission.json";
+
+            for (var i = 1; i < 13; i++)
+            {
+                if (i == 7)
+                {
+                    continue;
+                }
+
+                string locFile = localPath + @"\loc" + "\\" + $"M{i.ToString("D2")}_postmission.loc";
+                Process process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "hitmanbe",
+                        Arguments = $"\"{postmission}\" \"{locFile}\"",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                Console.WriteLine($"Processing: {postmission} -> {locFile}");
+                process.Start();
+                process.WaitForExit();
+
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                if (!string.IsNullOrEmpty(output))
+                    Console.WriteLine(output);
+                if (!string.IsNullOrEmpty(error))
+                    Console.WriteLine($"Error: {error}");
+            }
+        }
+
+        static void ReplaceMainNodeFromBloodMoneyFileToAllFiles()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            string jsonFolder = localPath + @"\json";
+            string mainUkr = localPath + @"\json\main\main.json";
+
+            var jsonUkr = File.ReadAllTextAsync(mainUkr, Encoding.UTF8).GetAwaiter().GetResult();
+
+            var ukr = JsonConvert.DeserializeObject<HLocClass>(jsonUkr);
+
+            string[] files = Directory.GetFiles(jsonFolder, "*.JSON");
+
+            for (int i1 = 0; i1 < files.Length; i1++)
+            {
+                string file = files[i1];
+                string fileName = Path.GetFileName(file);
+
+                if (fileName.ToUpperInvariant() == "M00_MAIN.JSON" || fileName.ToUpperInvariant().Contains("POSTMISSION"))
+                {
+                    continue;
+                }
+
+                var jsonText = File.ReadAllTextAsync(file, Encoding.UTF8).GetAwaiter().GetResult();
+
+                var hloc = JsonConvert.DeserializeObject<HLocClass>(jsonText);
+
+                hloc.children[0] = ukr;
+
+
+                //GoThrouClass(hloc);
+
+                //for (var i = 1; i < hloc.children.Count; i++)
+                //{
+                //    GoThrouClass(hloc.children[i]);
+                //}
+
+                var newJsonText = JsonConvert.SerializeObject(hloc, Newtonsoft.Json.Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+                string destinationFilePath = Path.Combine(jsonFolder, fileName);
+                File.WriteAllText(destinationFilePath, newJsonText, Encoding.UTF8);
+                Console.WriteLine($"{i1}/{files.Length}");
+            }
+        }
+
+        private static void GoThrouClass(HLocClass root)
+        {
+            if (root == null) return;
+            Stack<HLocClass> stack = new Stack<HLocClass>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                HLocClass current = stack.Pop();
+                if (!string.IsNullOrEmpty(current.value))
+                {
+                    current.value = "";//TranslateText(current.value, current.name);
+                }
+
+                if (current.children != null)
+                {
+                    for (int i = current.children.Count - 1; i >= 0; i--)
+                    {
+                        if (current.children[i] != null) // Переконуємося, що елемент не null
+                        {
+                            stack.Push(current.children[i]);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
